a16bf0ad74f50a7c5e2e2204fab76449cfae17f1
arijitlaik <arijit.netcom@gmail.com>
2015-12-07 05:55:05 +0530

    show the plotted data in a table

--- js/util.js
+++ js/util.js
@@ -15,10 +15,47 @@ var ur_popl = [];
 var ur_lnpl = [];
 var ur_selected = [];
 
+function insertdata(ur_in) {
+
+	var table = document.getElementById("dataout").getElementsByTagName('tbody')[0];
+
+	var row = table.insertRow(table.rows.length);
+
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	cell1.className = 'mdl-data-table__cell--non-numeric';
+
+	var dataType = "";
+	if (ur_in instanceof Line)
+		dataType = "Line";
+	else if (ur_in instanceof Plane)
+		dataType = "Plane";
+	else if (ur_in instanceof PoletoPlane)
+		dataType = "Pole to Plane";
+	else if (ur_in instanceof LineonPlane)
+		dataType = "Line on Plane";
+	else
+		dataType = "error";
+
+	cell1.innerHTML = dataType;
+	if (ur_in instanceof Plane) {
+		cell2.innerHTML = ur_in.strike;
+		cell3.innerHTML = ur_in.dip;
+	} else {
+		cell2.innerHTML = ur_in.trend;
+		cell3.innerHTML = ur_in.plunge;
+	}
+	componentHandler.upgradeElement(row);
+
+}
+
 function addPl() {
 	ur_pl.push(new Plane(+document.getElementById("st").value, +document.getElementById("dd").value, "blue", 0.5, "user_plane" + ur_pl.length));
+	insertdata(ur_pl[ur_pl.length - 1]);
 	if (document.getElementById("poto").checked) {
 		ur_popl.push(new PoletoPlane(ur_pl[ur_pl.length - 1], "orange", "user_poletoplane" + ur_popl.length));
+		insertdata(ur_popl[ur_popl.length - 1]);
 	}
 
 }
@@ -28,6 +65,7 @@ function addLn() {
 	document.getElementById(ur_ln[ur_ln.length - 1].plot.id).addEventListener("click", function () {
 		SelectThis(event);
 	});
+	insertdata(ur_ln[ur_ln.length - 1])
 }
 
 function SelectThis(event) {
@@ -38,10 +76,12 @@ function SelectThis(event) {
 
 function addPoPl() {
 	ur_popl.push(new PoletoPlane(new Plane(+document.getElementById("pst").value, +document.getElementById("pdd").value), "orange", "user_poletoplane" + ur_popl.length));
+	insertdata(ur_popl[ur_popl.length - 1]);
 }
 
 function addLnPl() {
 	ur_lnpl.push(new LineonPlane(new Plane(+document.getElementById("rst").value, +document.getElementById("rdd").value), document.getElementById("rpi").value, document.getElementById("ropfl").checked, "teal", "user_lineonplane" + ur_lnpl.length));
+	insertdata(ur_lnpl[ur_lnpl.length - 1])
 }
 
 function netOn() {
@@ -91,6 +131,11 @@ function rotateOl() {
 			document.getElementById(ur_lnpl[i].plot.id).setAttribute("transform", "rotate(" + deg + " " + center.x + " " + center.y + ")");
 	}
 }
+
+
+
+
+
 
 
 function rotateNet() {
