02aa327d0ad86851b5d7477448f464cda7611ff4
arijitlaik <arijit.netcom@gmail.com>
2015-12-12 20:22:06 +0530

    service worker

---
+++ sw.js
@@ -0,0 +1,40 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('SvgNet').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+				'/css/material.icons.css',
+				'/css/material.custom.css',
+				'/css/material.cyan-blue.min.css',
+        '/fonts/MaterialIcons-Regular.woff2',
+        '/fonts/MaterialIcons-Regular.woff',
+        '/fonts/MaterialIcons-Regular.ttf',
+        '/fonts/MaterialIcons-Regular.eot',
+        '/js/init.js',
+        '/js/svgnet.js',
+        '/js/util.js',
+        '/js/ui.js',
+        '/icon.png'
+      ]).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
