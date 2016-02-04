afb5005a494b117d47e675a1a6fec6a08e6b2a5b
arijitlaik <arijit.netcom@gmail.com>
2016-02-04 18:13:25 +0530

    revamp ui

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,29 @@
 var headerCheckHandler = function (event) {
-		var table = document.querySelector('table');
-		var boxes = table.querySelectorAll('tbody .mdl-data-table__select');
-		if (event.target.checked) {
-			for (var i = 0, length = boxes.length; i < length; i++) {
-				if(!boxes[i].querySelector('input').checked)
+	var table = document.querySelector('table');
+	var boxes = table.querySelectorAll('tbody .mdl-data-table__select');
+	if (event.target.checked) {
+		for (var i = 0, length = boxes.length; i < length; i++) {
+			if (!boxes[i].querySelector('input').checked)
 				boxes[i].querySelector('input').click();
-			}
-		} else {
-			for (var i = 0, length = boxes.length; i < length; i++) {
-				if(boxes[i].querySelector('input').checked)
-				boxes[i].querySelector('input').click();
-			}
 		}
-	};
+	} else {
+		for (var i = 0, length = boxes.length; i < length; i++) {
+			if (boxes[i].querySelector('input').checked)
+				boxes[i].querySelector('input').click();
+		}
+	}
+};
 document.getElementById("selectall").addEventListener('change', headerCheckHandler);
+//classList.toggle("content-hidden")
+
+var x = document.querySelectorAll(".toggle-card--content")
+for (var i = 0; i < x.length; i++) {
+	x[i].addEventListener('click', function (event) {
+		if (event.target.classList.contains("material-icons"))
+			event.target.parentElement.parentElement.parentElement.classList.toggle("content-hidden");
+		if (event.target.classList.contains("mdl-button") || (event.target.classList.contains("mdl-card__title-text")))
+			event.target.parentElement.parentElement.classList.toggle("content-hidden");
+		if (event.target.classList.contains("mdl-card__title"))
+			event.target.parentElement.classList.toggle("content-hidden");
+	});
+}
