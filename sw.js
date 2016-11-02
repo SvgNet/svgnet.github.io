ad7720b02f567c20ad3b8d0d10fa4f7b3b3072f9
arijitlaik <arijit.netcom@gmail.com>
2016-11-03 04:52:55 +0530

    sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('/cache-polyfill.js');
 const PREFIX = 'SvgNet';
-const VER = 'v3.0.3-beta';
+const VER = 'v3.0.5-alpha';
 const OFFLINE_CACHE = `${PREFIX}-${VER}`;
 var CACHE_URLS = [
     '/'
